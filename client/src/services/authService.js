210a3a80213a0f9a0f9984aa0aa877b98904efb3
Abhinav Karnatak <abhinavkarnatak2004@gmail.com>
2025-06-28 16:59:47 +0530

    Check

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,16 +1,16 @@
 import api from "./api";
 
 export const loginUser = async (credentials) => {
-  const response = await axios.post("/api/auth/login", credentials);
+  const response = await api.post("/auth/login", credentials);
   return response.data.data;
 };
 
 export const signupUser = async (userData) => {
-  const response = await axios.post("/api/auth/signup", userData);
+  const response = await api.post("/auth/signup", userData);
   return response.data.data;
 };
 
 export const verifyOtp = async (payload) => {
-  const response = await axios.post("/api/auth/verify", payload);
+  const response = await api.post("/auth/verify", payload);
   return response.data.data;
 };
